de471064ae7db4a4913e32a32f473e20c27364e3
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-08-14 13:53:43 +0900

    Fix: ./src files weren't loaded if Cache_Casual is installed with pear.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../src');
+set_include_path(dirname(__FILE__) . '/../src' . PATH_SEPARATOR . get_include_path());
 require_once 'Cache/Casual.php';
 require_once 'Cache/Casual/Container/Memory.php';
 require_once 'Cache/Casual/Container/File.php';
